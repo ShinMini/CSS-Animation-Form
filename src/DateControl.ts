bc3eb3f7480ed7387c4c371bdeb05515bb928dc1
mini <gusals121234@gmail.com>
2022-01-10 16:04:16 +0900

    [update] secondBranch at 16:04

--- src/DateControl.ts
+++ src/DateControl.ts
@@ -1,36 +1,30 @@
 /** @format */
 
 public class DateControl {
+  constructor(private FirstMeeTDate: string) {
+    private let DateControls {
+      private FMDate: string = FirstMeeTDate,
+    }
+  }
 
 /* firstMeet: save first meeting day(21.12.24),now: save now time value => in Date type */
 // {toNow: save now date time , toFirst: save first meeting date } => number type
 // PassedTime: calculate d-day time function 
-public PassedTime = function(FirstMeetTime: number): number {
-  {
-    const firstMeet: number= new Date(FirstMeetTime).getTime();
-    const NowTime: number= new Date().getTime();
+public PassedTime = function(FirstMeetTime: string): number {
+  {// dateTimeSetting: set times variable -> number(ms), firstMeet: set first meeting, nowTime: set now time
     const dateTimeSetting: number = (1000 * 60 * 60* 24) + 1;
+    const firstMeet: number= new Date(FirstMeetTime).getTime();
+    const nowTime: number= new Date().getTime();
   }
 
-  return this.NowTime - this.firstMeet / this.dateTimeSetting;
+  return Math.round((this.nowTime - this.firstMeet) / this.dateTimeSetting);
 }
-"2021-12-24"
-public const passedTimeResult: number = this.PassedTime(this.toNow, this.toFirst);
-
-// Math.round 함수를 이용해서, 결과값을 반올림해준 뒤, 변수에 저장.
-// 1000ms * 60second * 60minute * 24hour
-
-PassedDay = (x: number, y: number): number => {
-  return Math.round(x / y);
-}
-
-public passed = this.PassedDay(this.passedTimeResult, this.dateTimeSetting);
-// console.log(passedDay);
-// 저장한 값을 화면에 표시해줌,
+console.log(PassedTime("21.12.24"));
 
 const firstMeetingDay = firstYear + "." + firstMonth + "." + firstDate;
 
 function CalculateDay(greetingDay) {
+
   let greetingDayResult = toFirst + this.greetingDay;
 }
 
