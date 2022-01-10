eaa919b9e3d5200aad853de64af6feeb3df99aa3
mini <gusals121234@gmail.com>
2022-01-10 13:43:01 +0900

    [update] DatecontrolDot .ts file ,
    upload second branch before adjust update to main branch

--- src/DateControl.ts
+++ src/DateControl.ts
@@ -1,28 +1,33 @@
 /** @format */
 
 public class DateControl {
-// Date control functions
-// 첫만남 날짜 기록
+/* firstMeet: save first meeting day(21.12.24),now: save now time value => in Date type */
 public const firstMeet: Date = new Date("2021-12-24");
-// 현재 시간 기록
-public now: Date = new Date();
+public const NowTime: Date = new Date();
 
-// 첫만남과, 현재 시간을 number type으로 바꿔 계산하기 편하게 만들어줌.
-public const toNow: Number = this.now.getTime();
-public const toFirst: Number = this.firstMeet.getTime();
+// {toNow: save now date time , toFirst: save first meeting date } => number type
+public const toNow: number = this.NowTime.getTime();
+public const toFirst: number = this.firstMeet.getTime();
 
-// 만남 이후 D-Day 기록 계산 함수. 
-const passedTime = function(NowTime: Number, FirstMeetTime:  Number):Number{
-  let passedTimeFunctionResult: Number = this.NowTime- this.FirstMeetTime;
+// PassedTime: calculate d-day time function 
+public PassedTime = function(NowTime: number, FirstMeetTime:  number):number{
+
+  let passedTimeFunctionResult: number = NowTime - FirstMeetTime;
   return passedTimeFunctionResult;
 }
 
-public passedTimeResult: Number = this.passedTime(this.toNow, this.toFirst);
+public const passedTimeResult: number = this.PassedTime(this.toNow, this.toFirst);
+
+dateTimeSetting: number = (1000 * 60 * 60* 24) + 1;
 
 // Math.round 함수를 이용해서, 결과값을 반올림해준 뒤, 변수에 저장.
 // 1000ms * 60second * 60minute * 24hour
-public const passedDay: Number = Math.round(this.passedTimeResult / (1000 * 60 * 60 * 24)) + 1;
 
+PassedDay = (x: number, y: number): number => {
+  return Math.round(x / y);
+}
+
+public passed = this.PassedDay(this.passedTimeResult, this.dateTimeSetting);
 // console.log(passedDay);
 // 저장한 값을 화면에 표시해줌,
 
