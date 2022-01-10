58b88d12411cd4960ccca01ce0eb055cc1341cb0
mini <gusals121234@gmail.com>
2022-01-10 19:33:12 +0900

    [update] complete at laptop version -> optimizing code

--- src/DateControl.ts
+++ src/DateControl.ts
@@ -2,38 +2,49 @@
 
 public class DateControl {
   constructor(private FirstMeeTDate: string) {
-    private let DateControls {
-      private FMDate: string = FirstMeeTDate,
-    }
-  }
 
-/* firstMeet: save first meeting day(21.12.24),now: save now time value => in Date type */
-// {toNow: save now date time , toFirst: save first meeting date } => number type
-// PassedTime: calculate d-day time function 
-public PassedTime = function(FirstMeetTime: string): number {
-  {// dateTimeSetting: set times variable -> number(ms), firstMeet: set first meeting, nowTime: set now time
+    private let DateControls: string[] = [
+       FirstMeeTDate
+    ];
+  };
+
+public PassedTime = function(FMTD: string): number {
+  { // dateTimeSetting: set times variable -> number(ms), firstMeet: set first meeting, nowTime: set now time
     const dateTimeSetting: number = (1000 * 60 * 60* 24) + 1;
-    const firstMeet: number= new Date(FirstMeetTime).getTime();
+    const firstMeet: number= new Date(FMTD).getTime();
     const nowTime: number= new Date().getTime();
   }
-
   return Math.round((this.nowTime - this.firstMeet) / this.dateTimeSetting);
 }
-console.log(PassedTime("21.12.24"));
 
-const firstMeetingDay = firstYear + "." + firstMonth + "." + firstDate;
+// save passed time value
+public const savePassedTime: number = this.PassedTime(this.FirstMeeTDate);
+// const exampleValue: number = this.PassedTime("21.12.24");
+
+
+
+public FirstMeetingFunction = function(savePassedTime: Date): object {
+  const firstYear: number = savePassedTime.getFullYear();
+  const firstMonth: number = savePassedTime.getMonth() + 1;
+  const firstDate: number= savePassedTime.getDate();
+  const firstMeetingDay = firstYear + "." + firstMonth + "." + firstDate;
+
+  const firstDateSet: any = [
+    firstYear, 
+    firstMonth, 
+    firstDate, 
+    firstMeetingDay
+  ];
+  return firstDateSet;
+}
+
+const firstMeetingFunctionEx: any[] = [ this.FirstMeetingFunction(this.savePassedTime)];
 
 function CalculateDay(greetingDay) {
 
   let greetingDayResult = toFirst + this.greetingDay;
 }
 
-function firstMeetingFunction() {
-  const firstYear = firstMeet.getFullYear();
-  const firstMonth = firstMeet.getMonth() + 1;
-  const firstDate = firstMeet.getDate();
-  const firstMeetingDay = firstYear + "." + firstMonth + "." + firstDate;
-}
 const exampleValue = document.querySelector(".firstDays");
 exampleValue.innerText = firstMeetingDay;
 }
