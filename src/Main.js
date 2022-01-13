c39c949cda1f2389ec0247735eabd456f98e71c5
mini <gusals121234@gmail.com>
2022-01-13 16:33:34 +0900

    [update]

--- src/Main.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import DateCalculator from "./feat/DateCalculator.ts";
-import PannelControl from "./feat/PannelControl.ts"
-
-// let Panel = new PannelControl();
-
-console.log(Pannel.DotConeectContent());
---
+++ src/feat/IndexControl.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+const headerControl = (headerClass: string) => {
+     const header = document.getElementsByClassName(headerClass);
+
+     console.log(header);
+}
+
+headerControl('mainHeader');
