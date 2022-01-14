1698087fb37e88471cc352479756a89a690a34c9
mini <gusals121234@gmail.com>
2022-01-14 15:09:26 +0900

    [add] garbage variable
    add garbage variable for testing import feature working well

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import * as Date from "./feat/DateCalculator"
 import PannelControl from "./feat/PannelControl";
 import IndexControl from "./feat/IndexControl"
 
+IndexControl.
+
 // let Panel = new PannelControl();
