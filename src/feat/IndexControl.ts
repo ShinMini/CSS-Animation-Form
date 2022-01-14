19d2ff919bb2a478f9e17b69b7df11d184a54ea8
mini <gusals121234@gmail.com>
2022-01-14 14:33:02 +0900

    [update] import, export part

--- src/feat/IndexControl.ts
+++ src/feat/IndexControl.ts
@@ -1,9 +1,13 @@
 /** @format */
 
-const headerControl = (headerClass: string) => {
-     const header = document.getElementsByClassName(headerClass);
+export default class headerControl { 
 
+public headerValueReturn = function(headerClass: string): object {
+     const header: object = document.getElementsByClassName(headerClass);
      console.log(header);
-}
+          return header;
+ }    }
+
+
 
 headerControl('mainHeader');
