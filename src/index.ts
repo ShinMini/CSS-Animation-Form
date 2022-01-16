0ce5870a7be8a0dc617b7ca0212675b2ba8d358a
mini <gusals121234@gmail.com>
2022-01-16 15:20:56 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import * as Date from "./feat/DateCalculator"
 import PannelControl from "./feat/PannelControl";
 import IndexControl from "./feat/IndexControl"
 
-IndexControl.
 
 // let Panel = new PannelControl();
